0203ae2e98ac1ba2daf228f119846166b5edc733
SanketTops <sanket.tops@gmail.com>
2025-02-13 16:32:34 +0530

    String

---
+++ Module-1/string_example.py
@@ -0,0 +1,31 @@
+mystr = "this is python!"
+"""print(mystr)
+print(mystr[0])
+print(mystr[-1])
+print(mystr[0:4])  # slice - Range
+print(mystr[5:])
+print(mystr[:4])
+print(len(mystr))"""
+
+# ------------------------------ #
+# String Methods
+print(mystr)
+# print(mystr.strip())
+# print(mystr.lower())
+# print(mystr.upper())
+# print(mystr.replace("This", "That"))
+# print(mystr.split())
+# print(mystr.capitalize())
+# print(mystr.casefold())
+# print(mystr.count("i"))
+# print(mystr.count("This"))
+# print(mystr.endswith("."))
+# print(mystr.startswith("This"))
+# print(mystr.find("python"))
+# print(mystr.index("p"))
+# print(mystr.isalpha())
+# print(mystr.isdigit())
+# print(mystr.isalnum())
+# print(mystr.islower())
+# print(mystr.isupper())
+print(mystr.title())
