42e34990294c8de20b2b708857de41c4cbccc40b
SanketTops <sanket.tops@gmail.com>
2025-02-14 16:30:18 +0530

    Collection

---
+++ Module-2_Collection/list_example.py
@@ -0,0 +1,47 @@
+data = ["python", "java", "php", "kotlin", "c++"]
+"""print(data)
+print(data[0])
+print(data[-1])
+print(data[0:3])
+print(data[2:])
+print(data[:3])
+print(len(data))"""
+
+# ---------------------------------- #
+# print(data)
+
+"""if "C++" in data:
+    print("Yes...")
+else:
+    print("No..")"""
+
+"""for i in data:
+    print(i)"""
+
+
+# -------------------------------------- #
+# print(data)
+# data[3] = "android"
+print(data)
+# data.append("dbms")
+# data.insert(2, "dbms")
+# data.pop()
+# data.pop(1)
+# data.remove("php")
+# data.clear()
+# del data[0]
+# del data
+# data.reverse()
+# data.sort()
+# data.sort(reverse=True)
+# newdata = data.copy()
+# print(data)
+# print(newdata)
+
+
+newdata = ["html", "css", "js", "react"]
+print(newdata)
+
+# print(data + newdata)
+data.extend(newdata)
+print(data)
