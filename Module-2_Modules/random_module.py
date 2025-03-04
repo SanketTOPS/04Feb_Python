b25d785808eeb3df47c2242b7595be2fcc91525b
SanketTops <sanket.tops@gmail.com>
2025-03-04 16:19:27 +0530

    Module - Random

---
+++ Module-2_Modules/random_module.py
@@ -0,0 +1,12 @@
+import random
+
+# x = random.random()
+# x = random.randint(1111, 9999)
+# x = random.randrange(111111, 999999, 120)
+# print(x)
+
+captcha = ["Hb8w", "Nsi9", "Mxn0", "5cnb", "Ny24", "Ncu8", "Rx3a", "7jh2", "Vx4a"]
+# x = random.choice(captcha)
+random.shuffle(captcha)
+# print(x)
+print(captcha)
