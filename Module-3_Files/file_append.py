7ede50dc3d0c9b89357f8000952147a3b2e4b677
SanketTops <sanket.tops@gmail.com>
2025-03-07 16:21:24 +0530

    Files

---
+++ Module-3_Files/file_append.py
@@ -0,0 +1,8 @@
+file = open("hello.txt", "a")
+
+id = input("Enter an ID:")
+name = input("Enter a Name:")
+city = input("Enter a City")
+
+
+file.write(f"ID:{id}\nName:{name}\nCity:{city}")
---
+++ Module-3_Files/file_create.py
@@ -0,0 +1,4 @@
+# open("temp.txt", "x")  # x mode for file create
+# open("new.txt", "w")  # w mode for file create + write
+open("hello.txt", "a")  # a mode for file create + append
+print("file is created!")
