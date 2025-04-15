2c9b2b162cf7690ff7ec8658a5fb6b755b727ce6
SanketTops <sanket.tops@gmail.com>
2025-04-15 16:38:18 +0530

    CRUD

--- Django/DBProject/myapp/views.py
+++ Django/DBProject/myapp/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .forms import *
 
 
 # Create your views here.
 def index(request):
+    if request.method == "POST":
+        form = studForm(request.POST)
+        if form.is_valid():
+            form.save()
+            print("Record Inserted!")
+        else:
+            print(form.errors)
     return render(request, "index.html")
