775d9e985059039c80129625b3c0ecfdaf043e0b
SanketTops <sanket.tops@gmail.com>
2025-05-06 14:50:08 +0530

    Template Extends

---
+++ Django/BatchProject/adminapp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def adminhome(request):
+    return render(request, "adminhome.html")
---
+++ Django/BatchProject/noteapp/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from noteapp import views
+
+urlpatterns = [
+    path("", views.index),
+    path("notes/", views.notes, name="notes"),
+    path("profile/", views.profile, name="profile"),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
+]
