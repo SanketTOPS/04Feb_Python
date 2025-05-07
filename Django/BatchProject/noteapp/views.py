66ec91c6aa6e5936514e1c8348409ff4304d90e8
SanketTops <sanket.tops@gmail.com>
2025-05-07 14:42:21 +0530

    Signup

--- Django/BatchProject/noteapp/views.py
+++ Django/BatchProject/noteapp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import *
+from .models import *
 
 
 # Create your views here.
@@ -20,3 +22,25 @@ def about(request):
 
 def contact(request):
     return render(request, "contact.html")
+
+
+def login(request):
+    return render(request, "login.html")
+
+
+def signup(request):
+    msg = ""
+    if request.method == "POST":
+        form = UsersignupForm(request.POST)
+        username = ""
+        try:
+            if form.is_valid():
+                username = Usersignup.objects.get(username=request.POST["username"])
+                username = form.cleaned_data.get(username)
+                print("Username is already exists!")
+                msg = "Username is already exists!"
+        except Usersignup.DoesNotExist:
+            form.save()
+            print("Signup Successfully!")
+            return redirect("login")
+    return render(request, "signup.html", {"msg": msg})
