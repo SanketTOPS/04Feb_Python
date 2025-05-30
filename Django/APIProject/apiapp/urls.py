706b6c2ddd324b63bdd89a7502b74a832fe52956
SanketTops <sanket.tops@gmail.com>
2025-05-30 14:55:33 +0530

    Full API CRUD

--- Django/APIProject/apiapp/urls.py
+++ Django/APIProject/apiapp/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from apiapp import views
 
 urlpatterns = [
-    path("", views.getalldata),
+    path("", views.stdata),
+    path("getalldata/", views.getalldata),
+    path("getstid/<int:id>", views.getstid),
+    path("deletestid/<int:id>", views.deletestid),
+    path("savedata/", views.savedata),
+    path("updatedata/<int:id>", views.updatedata),
 ]
--- Django/APIProject/apiapp/views.py
+++ Django/APIProject/apiapp/views.py
@@ -3,6 +3,8 @@ from .models import *
 from .serializers import *
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+import requests
 
 
 # Create your views here.
@@ -11,3 +13,64 @@ def getalldata(request):
     stdata = Studinfo.objects.all()
     serial = StudSerial(stdata, many=True)
     return Response(data=serial.data)
+
+
+@api_view(["GET"])
+def getstid(request, id):
+    try:
+        stid = Studinfo.objects.get(id=id)
+    except Studinfo.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    serial = StudSerial(stid)
+    return Response(data=serial.data)
+
+
+@api_view(["DELETE", "GET"])
+def deletestid(request, id):
+    try:
+        stid = Studinfo.objects.get(id=id)
+    except Studinfo.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == "GET":
+        serial = StudSerial(stid)
+        return Response(data=serial.data)
+    if request.method == "DELETE":
+        Studinfo.delete(stid)
+        return Response(status=status.HTTP_202_ACCEPTED)
+
+
+@api_view(["POST"])
+def savedata(request):
+    if request.method == "POST":
+        serial = StudSerial(data=request.data)
+        if serial.is_valid():
+            serial.save()
+            return Response(status=status.HTTP_201_CREATED)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["PUT", "GET"])
+def updatedata(request, id):
+    try:
+        stid = Studinfo.objects.get(id=id)
+    except Studinfo.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == "GET":
+        serial = StudSerial(stid)
+        return Response(data=serial.data)
+    if request.method == "PUT":
+        serial = StudSerial(data=request.data, instance=stid)
+        if serial.is_valid():
+            serial.save()
+            return Response(status=status.HTTP_202_ACCEPTED)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+def stdata(request):
+    url = "http://127.0.0.1:8000/getalldata/"
+    req = requests.get(url)
+    data = req.json()
+    print(data)
+    return render(request, "index.html", {"data": data})
